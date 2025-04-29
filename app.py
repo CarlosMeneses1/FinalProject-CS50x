33e0f9fc6b8781828f6071f9c02d445e2447d7d4
javie <javier.alvarado3011@gmail.com>
2025-04-28 22:49:27 -0400

    Navbar desing

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Configure application
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world" 
+    return "Hello, world"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+ 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
